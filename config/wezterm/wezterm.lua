905b1f0d1ab25941c2a5d0c232fafa4d712bfe18
Alex Shneyderman <a.shneyderman@gmail.com>
2024-11-15 07:17:26 -0500

    chore(wezterm): add shortcut to create new workspace; make tabs visible

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -11,4 +11,36 @@ local success, stdout, stderr = wezterm.run_child_process({ os.getenv("SHELL"), 
 local selected_theme = stdout:gsub("%s+", "") -- Remove all whitespace characters including newline
 config.color_scheme = themes[selected_theme]
 
+wezterm.on('update-right-status', function(window, pane)
+  window:set_right_status(window:active_workspace())
+end)
+
+config.keys = {
+  -- Prompt for a name to use for a new workspace and switch to it.
+  {
+    key = "N",
+    mods = "SHIFT|CTRL|ALT",
+    action = wezterm.action.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = 'Bold' } },
+        { Foreground = { AnsiColor = 'Fuchsia' } },
+        { Text = 'Enter name for new workspace' },
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:perform_action(
+            wezterm.action.SwitchToWorkspace {
+              name = line,
+            },
+            pane
+          )
+        end
+      end),
+    },
+  },
+}
+
 return config
